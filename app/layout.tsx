d8c0a0f7d5b8170f82114a132adf60b0892f5b29
JAZ LOH <jazloh@MacBook-Pro.local>
2026-05-21 18:31:16 +0800

    Add PWA support

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import Script from 'next/script';
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -17,7 +18,12 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-black text-white antialiased`}>{children}</body>
+      <body className={`${inter.className} bg-black text-white antialiased`}>
+        {children}
+        <Script id="sw-register" strategy="afterInteractive">
+          {`if('serviceWorker' in navigator) navigator.serviceWorker.register('/sw.js');`}
+        </Script>
+      </body>
     </html>
   );
 }
